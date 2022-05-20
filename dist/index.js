ba411788a3aeac6bee9a3fcfacae4a56cdf70994
Andreea-Laura Ungureanu <andreea.ungureanu@visma.com>
2022-05-20 12:25:34 +0300

    wip

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,11 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -104,38 +108,33 @@ function validateFilenames(path, pattern) {
         const opendir = fs_1.default.promises.opendir;
         const failedFiles = [];
         let totalFilesAnalyzed = 0;
+        const dir = yield opendir(path);
+        console.log('Verification starting...');
         try {
-            const dir = yield opendir(path);
-            console.log('Verification starting...');
+            for (var dir_1 = __asyncValues(dir), dir_1_1; dir_1_1 = yield dir_1.next(), !dir_1_1.done;) {
+                const dirent = dir_1_1.value;
+                if (dirent.isDirectory()) {
+                    continue;
+                }
+                totalFilesAnalyzed++;
+                if (pattern.test(dirent.name)) {
+                    console.log(`  ✔️  ${dirent.name}`);
+                }
+                else {
+                    console.log(`  ❌  ${dirent.name}`);
+                    failedFiles.push(dirent.name);
+                }
+            }
+        }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
+        finally {
             try {
-                for (var dir_1 = __asyncValues(dir), dir_1_1; dir_1_1 = yield dir_1.next(), !dir_1_1.done;) {
-                    const dirent = dir_1_1.value;
-                    if (dirent.isDirectory()) {
-                        continue;
-                    }
-                    totalFilesAnalyzed++;
-                    if (pattern.test(dirent.name)) {
-                        console.log(`  ✔️  ${dirent.name}`);
-                    }
-                    else {
-                        console.log(`  ❌  ${dirent.name}`);
-                        failedFiles.push(dirent.name);
-                    }
-                }
+                if (dir_1_1 && !dir_1_1.done && (_a = dir_1.return)) yield _a.call(dir_1);
             }
-            catch (e_1_1) { e_1 = { error: e_1_1 }; }
-            finally {
-                try {
-                    if (dir_1_1 && !dir_1_1.done && (_a = dir_1.return)) yield _a.call(dir_1);
-                }
-                finally { if (e_1) throw e_1.error; }
-            }
-            console.log('Verification finished.');
-            console.log(`ℹ️  Files analyzed: \t${totalFilesAnalyzed}`);
+            finally { if (e_1) throw e_1.error; }
         }
-        catch (error) {
-            throw new Error('Execution failed, see log above. ❌');
-        }
+        console.log('Verification finished.');
+        console.log(`ℹ️  Files analyzed: \t${totalFilesAnalyzed}`);
         if (failedFiles.length) {
             throw new Error(`${failedFiles.length} files not matching the pattern were found, see log above. ❌`);
         }
@@ -5600,9 +5599,17 @@ AbortError.prototype = Object.create(Error.prototype);
 AbortError.prototype.constructor = AbortError;
 AbortError.prototype.name = 'AbortError';
 
+const URL$1 = Url.URL || whatwgUrl.URL;
+
 // fix an issue where "PassThrough", "resolve" aren't a named export for node <10
 const PassThrough$1 = Stream.PassThrough;
-const resolve_url = Url.resolve;
+
+const isDomainOrSubdomain = function isDomainOrSubdomain(destination, original) {
+	const orig = new URL$1(original).hostname;
+	const dest = new URL$1(destination).hostname;
+
+	return orig === dest || orig[orig.length - dest.length - 1] === '.' && orig.endsWith(dest);
+};
 
 /**
  * Fetch function
@@ -5690,7 +5697,19 @@ function fetch(url, opts) {
 				const location = headers.get('Location');
 
 				// HTTP fetch step 5.3
-				const locationURL = location === null ? null : resolve_url(request.url, location);
+				let locationURL = null;
+				try {
+					locationURL = location === null ? null : new URL$1(location, request.url).toString();
+				} catch (err) {
+					// error here can only be invalid URL in Location: header
+					// do not throw when options.redirect == manual
+					// let the user extract the errorneous redirect URL
+					if (request.redirect !== 'manual') {
+						reject(new FetchError(`uri requested responds with an invalid redirect URL: ${location}`, 'invalid-redirect'));
+						finalize();
+						return;
+					}
+				}
 
 				// HTTP fetch step 5.5
 				switch (request.redirect) {
@@ -5737,6 +5756,12 @@ function fetch(url, opts) {
 							timeout: request.timeout,
 							size: request.size
 						};
+
+						if (!isDomainOrSubdomain(request.url, locationURL)) {
+							for (const name of ['authorization', 'www-authenticate', 'cookie', 'cookie2']) {
+								requestOpts.headers.delete(name);
+							}
+						}
 
 						// HTTP-redirect fetch step 9
 						if (res.statusCode !== 303 && request.body && getTotalBytes(request) === null) {
